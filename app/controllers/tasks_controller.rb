d481681184f77539d7a648ad536a20352531baba
youhei.iwahashi <y.iwahashi0906@gmail.com>
2022-10-03 11:24:20 +0000

    task-list update

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,10 @@
 class TasksController < ApplicationController
    before_action :set_task, only: [:show, :edit, :update, :destroy]
+   before_action :require_user_logged_in
+   before_action :correct_user, only: [:destroy]
    
   def index
-    @tasks = Task.all
+    @pagy, @tasks = pagy(current_user.tasks.order(id: :desc))
   end
 
   def show
@@ -13,7 +15,7 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
 
     if @task.save
       flash[:success] = 'Task が正常に投稿されました'
@@ -50,8 +52,17 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
   
-  # Strong Parameter
+  
+    # Strong Parameter
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content, :status)
+  end
+  
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 end
---
+++ db/migrate/20220923070815_adduser_id_to_tasks.rb
@@ -0,0 +1,4 @@
+class AdduserIdToTasks < ActiveRecord::Migration[6.1]
+  def change
+  end
+end
